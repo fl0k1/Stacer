ccbcc6b6a2088861b08e893705cf07f99f2ced8b
fl0k1 <michele.carnova@gmail.com>
2017-09-07 03:24:50 +0200

    Create stacer_it.ts
    I added the Italian language. Hope this help :)

---
+++ translations/stacer_it.ts
@@ -0,0 +1,63 @@
+        <source>Caches delle Applicazioni</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/SystemCleaner/system_cleaner_page.ui" line="327"/>
+        <source>Cestino</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/SystemCleaner/system_cleaner_page.ui" line="365"/>
+        <source>Cache dei Pacchetti</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/SystemCleaner/system_cleaner_page.ui" line="473"/>
+        <source> Indietro</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/SystemCleaner/system_cleaner_page.cpp" line="32"/>
+        <source>Nome File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/SystemCleaner/system_cleaner_page.cpp" line="32"/>
+        <source>Dimensioni</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/SystemCleaner/system_cleaner_page.cpp" line="285"/>
+        <source>%1 eliminati.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>UninstallerPage</name>
+    <message>
+        <location filename="../stacer/Pages/Uninstaller/uninstallerpage.ui" line="228"/>
+        <source>Pacchetti di Sistema Installati</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Uninstaller/uninstallerpage.ui" line="271"/>
+        <source>Cerca...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Uninstaller/uninstallerpage.ui" line="178"/>
+        <source>Pacchetto installato non trovato</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Uninstaller/uninstallerpage.ui" line="132"/>
+        <source>Disinstalla Selezionati</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Uninstaller/uninstaller_page.cpp" line="65"/>
+        <source>Pacchetti di Sistema Installati (%1)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
